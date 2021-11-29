2dba4967481ce1ecd4195c9cf100a0171092e2ad
Ivan Ovsyannikov <32175074+ivanovs588@users.noreply.github.com>
2021-11-29 14:35:44 +0300

    Create Упражнение_2.py

---
+++ 11/Упражнение_2.py
@@ -0,0 +1,34 @@
+import os, re
+import threading
+import random
+import time
+import sys
+
+def thread_ip():
+    with lock: 
+    
+        global suffix
+        old_suffix = suffix
+        ip = "192.168.178." + str(suffix)
+        suffix = old_suffix + 1
+        ping_out = os.popen("ping -q -c2 " + ip, "r")  
+        print("... pinging ", ip)
+        while True:
+            line = ping_out.readline()
+            if not line:
+                break
+            n_received = received_packages.findall(line)
+            if n_received:
+                print(ip + ": " + status[int(n_received[0])])
+    
+
+
+lock = threading.Lock()
+suffix = 20
+received_packages = re.compile(r"(\d) received")
+status = ("no response", "alive but losses", "alive")
+threads = [threading.Thread(target=thread_ip) for _ in range(20,30)]
+for thread in threads:
+    thread.start()
+for thread in threads:
+    thread.join()
